7839bd74c7c24fbb8463e629ce646ba4ba91d8fc
Sevengex <lduarte10804@gmail.com>
2025-08-10 02:26:46 -0300

    Agrego elementos al footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,23 @@
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: "lightcoral" }}>
-      <p>Sitio desarrollado por <a href="https://www.linkedin.com/in/leonardo-joaquin-duarte-lopez" target="_blank">Leonardo Duarte</a></p>
-    </footer>
+    <>
+      <footer style={{ backgroundColor: "lightcoral" }}>
+        <div><h2>Mi Tienda</h2>
+          <a>Tu tu tienda online de confianza con los majores productos y atencion personalizada</a>
+        </div>
+        <div>
+          <h3>Enlaces Utiles</h3>
+          <li>Inicio</li>
+          <li>Sobre Nosotros</li>
+          <li>Iniciar Sesion</li>
+          <li>Registrarse</li>
+        </div>
+        <div>
+          <h3>Contacto</h3>
+          <p>Sitio desarrollado por <a href="https://www.linkedin.com/in/leonardo-joaquin-duarte-lopez" target="_blank">Leonardo Duarte</a></p>
+        </div>
+      </footer>
+    </>
   )
 }
 
